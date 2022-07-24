11178266a1c0a4d99d463f81675f768aab7e7a76
Jason <oblitorum@gmail.com>
2022-07-24 11:46:51 +0800

    Refactor remote-instances-list page

--- src/app/remote-instances-list/remote-instances-list.service.ts
+++ src/app/remote-instances-list/remote-instances-list.service.ts
@@ -1,27 +1,35 @@
 import {Injectable} from '@angular/core';
-import {RemoteInstance, RemoteInstanceNode, RemoteInstanceService} from '../add-remote-instances/add-remote-instance.service';
+import {NodeInstance, NodeInstanceService} from '../add-remote-instances/add-remote-instance.service';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
+
+export const ClientRegion = "client"
 
 @Injectable()
 export class RemoteInstancesListService {
-
   private headers = new HttpHeaders({ 'Content-Type': 'application/json' });
 
   constructor(private http: HttpClient) {
   }
 
-  async getList(): Promise<RemoteInstance[]> {
-    const url = `/managed/v0/remote`;
+  async getList(): Promise<NodeInstance[]> {
+    const url = `/managed/v0/nodes`;
     const response = await this.http
       .get(url, { headers: this.headers })
       .toPromise();
-    return response['instances'] as RemoteInstance[];
+    return response['instances'] as NodeInstance[];
   }
 
-  async disable(node: RemoteInstanceNode, service: RemoteInstanceService): Promise<{}> {
-    const url = `/managed/v0/${service.type}/${node.id}`;
+  async disable(node: NodeInstance): Promise<{}> {
+    const url = `/managed/v0/nodes/${node.name}`;
     return await this.http
       .delete(url, {headers: this.headers})
+      .toPromise();
+  }
+
+  async disableService(node: string, service: NodeInstanceService): Promise<{}> {
+    const url = `/managed/v0/nodes/${node}/services`
+    return await this.http
+      .delete(url, { headers: this.headers, body: { id: service.id, type: service.type } })
       .toPromise();
   }
 }
