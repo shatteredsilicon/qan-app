50ff68cdb0d03665cb729b44b13f57599d6349ba
Jason <oblitorum@gmail.com>
2022-09-28 23:07:19 +0800

    Set ssm version at build time

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   demoHosts: ['163.172.51.248', 'pmmdemo.percona.com'],
-  version: require('../../package.json').version
+  version: require('../../env.json').version
 };
